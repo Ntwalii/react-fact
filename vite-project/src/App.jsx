34bdd020819844f31eb0d8e81b7053d52a773bca
ntwalii <ntwaliaubin@gmail.com>
2024-07-19 16:25:18 +0200

    React initial project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "./components/Main"
+import NavBar from "./components/NavBar"
+
+export default function App(){
+  return(
+    <>
+    <NavBar />
+    <Main />
+    </>
+  )
+}
+
